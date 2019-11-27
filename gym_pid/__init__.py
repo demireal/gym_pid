23b06c15790bcb795a9e0164b5720c4f58fa90e7
levi97 <30956324+levi97@users.noreply.github.com>
2019-11-27 20:32:47 +0300

    Update __init__.py

--- gym_pid/__init__.py
+++ gym_pid/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='pid-v0',
-    entry_point='gym_navigate2D.envs:pidEnv',
+    entry_point='gym_pid.envs:pidEnv',
 )
 register(
     id='pid-extrahard-v0',
